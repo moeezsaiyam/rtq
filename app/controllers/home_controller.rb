c8c6f31ebb12466e7545083ed5ed090720ebb87f
Zain Tariq <zanetariq@gmail.com>
2012-02-28 11:57:48 +0500

    Qick Search has been added and CSS added to show more button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,18 +6,24 @@ class HomeController < ApplicationController
   end
 
   def search
-    if params[:search_terms][:search_topic].blank? && params[:search_terms][:search_sub_topic].blank?
-      @topics = Topic.search params[:search_term]
-      @sub_topics = SubTopic.search params[:search_term]
-      @questions = Question.search params[:search_term]
+    unless params[:quick_search_term].blank?
+      @topics = Topic.search params[:quick_search_term]
+      @sub_topics = SubTopic.search params[:quick_search_term]
+      @questions = Question.search params[:quick_search_term]
     else
-      if params[:search_terms][:search_sub_topic].blank?
-        @sub_topics = SubTopic.search params[:search_term], :with => {:topic_id => params[:search_terms][:search_topic].to_i}
+      if params[:search_terms][:search_topic].blank? && params[:search_terms][:search_sub_topic].blank?
+        @topics = Topic.search params[:search_term]
+        @sub_topics = SubTopic.search params[:search_term]
         @questions = Question.search params[:search_term]
       else
-        @questions = Question.search params[:search_term], :with => {:sub_topic_id => params[:search_terms][:search_sub_topic].to_i}
+        if params[:search_terms][:search_sub_topic].blank?
+          @sub_topics = SubTopic.search params[:search_term], :with => {:topic_id => params[:search_terms][:search_topic].to_i}
+          @questions = Question.search params[:search_term]
+        else
+          @questions = Question.search params[:search_term], :with => {:sub_topic_id => params[:search_terms][:search_sub_topic].to_i}
+        end
       end
     end
   end
-
+  
 end
