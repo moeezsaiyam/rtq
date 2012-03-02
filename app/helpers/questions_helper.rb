4b20fa735e12d73b57dee3635b12ec9028cf3dab
Zain Tariq <zanetariq@gmail.com>
2012-03-02 13:21:44 +0500

    Breadcrumb added to browse button and null blocks checks of popular and latest blocks

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,5 +4,6 @@ module QuestionsHelper
     html = "<div class='bread-crumb'> <a href='/'> Home </a> > <a href='/#{question.sub_topic.topic.name_slug}'> #{question.sub_topic.topic.name} </a> > <a href='/#{question.sub_topic.topic.name_slug}/#{question.sub_topic.name_slug}'> #{question.sub_topic.name} </a></div>"
     return html
   end
-
-end
+  end
+  
+ 
