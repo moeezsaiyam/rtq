1d13bf1bc42578415f299f8b772b3e0c928a2684
Zain Tariq <zanetariq@gmail.com>
2012-04-03 10:39:22 +0500

    Cross browser checking has been done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,7 +46,7 @@ module ApplicationHelper
      addthis_html='
    <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-<a class="addthis_button_preferred_1"></a>
+h<a class="addthis_button_preferred_1"></a>
 <a class="addthis_button_preferred_2"></a>
 <a class="addthis_button_preferred_3"></a>
 <a class="addthis_button_preferred_4"></a>
