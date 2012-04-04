7d245bfeb8a0e095e7f53c26eea8dbde418a416e
Zain Tariq <zanetariq@gmail.com>
2012-04-04 17:49:41 +0500

    Default issue has been resolved

--- app/controllers/verse_controller.rb
+++ app/controllers/verse_controller.rb
@@ -3,45 +3,44 @@ class VerseController < ApplicationController
   def single_verse
    question_id = params[:prev].split("/").last
    @question = Question.find(:first, :conditions => ['id = ? OR quest_slug = ?', question_id, question_id])
-   puts @question.inspect
    @verse = Verse.find(params[:id])
    @prev_verses = @verse.previous_verses(3).reverse!
    @next_verses = @verse.next_verses(3)
-   @translation = Translation.find_by_default(1)
-   if params[:trans]
-     trans = params[:trans]
-   end
-   if trans.blank?
-     trans = [@translation.table_nam]
-   end
+   @translation = Translation.find_by_default(1) unless request.referrer.split("/").include?("verse_context")
+    unless @translation.blank? && params[:trans].blank?
+     trans = params[:trans] if params[:trans]
+     trans = [@translation.table_nam] if trans.blank?
      @trans_verses = Hash.new
-     trans.each do|t|
-     @trans_name = Translation.find_by_table_nam(t)
-     name = @trans_name.name
-   if @trans_verses.has_key?(@verse.id)
-     @trans_verses[@verse.id] = [ @trans_verses[@verse.id],'<h1>'+name + '<br>'+'</h1>', @verse.translate_to_by(t)[0].text+'<br><br>']
-   end
-     @trans_verses[@verse.id] = ['<h1>'+name + '<br>'+'</h1>',@verse.translate_to_by(t)[0].text+'<br><br>'] unless @trans_verses.has_key?(@verse.id)
-     @prev_verses.each do|v|
-      if @trans_verses.has_key?(v.id)
-     @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>']
-      end
-     @trans_verses[v.id] = ['<h1>'+name + '<br>'+'</h1>',v.translate_to_by(t)[0].text+ '<br><br>'] unless @trans_verses.has_key?(v.id)
-     end
-     @next_verses.each do|v|
-      if @trans_verses.has_key?(v.id)
-        @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+ name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>']
-      end
-          @trans_verses[v.id] = ['<h1>'+ name + '<br>'+'</h1>',v.translate_to_by(t)[0].text+'<br><br>'] unless @trans_verses.has_key?(v.id)
+     puts"///////////////////////////"
+     puts trans.inspect
+       trans.each do|t|
+       @trans_name = Translation.find_by_table_nam(t)
+       name = @trans_name.name
+        if @trans_verses.has_key?(@verse.id)
+          @trans_verses[@verse.id] = [ @trans_verses[@verse.id],'<h1>'+name + '<br>'+'</h1>', @verse.translate_to_by(t)[0].text+'<br><br>']
         end
-      end 
-  end
+        @trans_verses[@verse.id] = ['<h1>'+name + '<br>'+'</h1>',@verse.translate_to_by(t)[0].text+'<br><br>'] unless @trans_verses.has_key?(@verse.id)
+          @prev_verses.each do|v|
+            if @trans_verses.has_key?(v.id)
+            @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>']
+            end
+          @trans_verses[v.id] = ['<h1>'+name + '<br>'+'</h1>',v.translate_to_by(t)[0].text+ '<br><br>'] unless @trans_verses.has_key?(v.id)
+          end
+          @next_verses.each do|v|
+            if @trans_verses.has_key?(v.id)
+              @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+ name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>']
+            end
+            @trans_verses[v.id] = ['<h1>'+ name + '<br>'+'</h1>',v.translate_to_by(t)[0].text+'<br><br>'] unless @trans_verses.has_key?(v.id)
+          end
+      end
+   end    
+ end
 
   def show
     question_id = params[:prev].split("/").last
     @question = Question.find(:first, :conditions => ['id = ? OR quest_slug = ?', question_id, question_id])
     @verse = Verse.find(params[:range])
-    @translation = Translation.find_by_default(1) unless request.referrer.match("verse")
+    @translation = Translation.find_by_default(1) unless request.referrer.split("/").include?("verse")
     return head(404) unless @verse
     @verses_range = Verse.from_to_verses(params[:id],params[:range])
     unless @translation.blank? && params[:trans].blank?
