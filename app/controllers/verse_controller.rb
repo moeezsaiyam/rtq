ec6d649ed735872a177ca7d2de75a431202dcca3
Usman Ali <usman.ali@square63.com>
2012-04-04 16:39:41 +0500

    Translation : Show only first time

--- app/controllers/verse_controller.rb
+++ app/controllers/verse_controller.rb
@@ -35,34 +35,29 @@ class VerseController < ApplicationController
           @trans_verses[v.id] = ['<h1>'+ name + '<br>'+'</h1>',v.translate_to_by(t)[0].text+'<br><br>'] unless @trans_verses.has_key?(v.id)
         end
       end 
-      
-   
   end
+
   def show
     question_id = params[:prev].split("/").last
     @question = Question.find(:first, :conditions => ['id = ? OR quest_slug = ?', question_id, question_id])
     @verse = Verse.find(params[:range])
-    @translation = Translation.find_by_default(1)
+    @translation = Translation.find_by_default(1) unless request.referrer.match("verse")
     return head(404) unless @verse
-     @verses_range = Verse.from_to_verses(params[:id],params[:range])
-     if params[:trans]
-     trans = params[:trans]
-     end
-     if trans.blank? 
-      trans = [@translation.table_nam]
-     end
-     @trans_verses = Hash.new
+    @verses_range = Verse.from_to_verses(params[:id],params[:range])
+    unless @translation.blank? && params[:trans].blank?
+      trans = params[:trans] if params[:trans]
+      trans = [@translation.table_nam] if trans.blank?
+      @trans_verses = Hash.new
       trans.each do|t|
-      @trans_name = Translation.find_by_table_nam(t)
-      name = @trans_name.name
+        @trans_name = Translation.find_by_table_nam(t)
+        name = @trans_name.name
         @verses_range.each do|v|
-          if @trans_verses.has_key?(v.id)
-            @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>']
-          end
+          @trans_verses[v.id] = [ @trans_verses[v.id],'<h1>'+name + '<br>'+'</h1>', v.translate_to_by(t)[0].text + '<br><br>'] if @trans_verses.has_key?(v.id)
           @trans_verses[v.id] = ['<h1>'+name + '<br>'+'</h1>',v.translate_to_by(t)[0].text + '<br><br>'] unless @trans_verses.has_key?(v.id)
         end
-      end 
-     end
+      end
+    end
+  end
 
   def get_context
     verse = Verse.find_by_id params[:verse_id]
@@ -72,4 +67,4 @@ class VerseController < ApplicationController
        @verses.push(Verse.find id)
     end
   end
-  end
+end
