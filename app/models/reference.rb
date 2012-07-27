25b1c3dbbe7848c0861fdfb27642a25e33405d5c
Tauqeer Ahmad <tauqeer.ahmad@square63.com>
2012-07-27 16:32:16 +0500

    Edit questions: errors are displayed on invalid references.

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,19 +1,19 @@
 class Reference < ActiveRecord::Base
 belongs_to :question
 
-validates_presence_of :issue, :from, :to
+
 
   def validate
-    
+
     unless self.from.blank?
       record_tags = Array.new
       tags = self.from.split(",")
       tags.each do |tag|
-      sura=tag.split(":")[0]
-      aya=tag.split(":")[1]
-      verse = Verse.find_by_sura_and_aya(sura,aya)
-      return errors.add(:tags,"No Such reference exist") if verse.blank?
-      record_tags.push(verse.id)
+        sura=tag.split(":")[0]
+        aya=tag.split(":")[1]
+        verse = Verse.find_by_sura_and_aya(sura,aya)
+        return errors.add(:tags,"No Such reference exist") if verse.blank?
+        record_tags.push(verse.id)
       end
       self.verse_ids = record_tags.join(",")
     end
@@ -21,11 +21,11 @@ validates_presence_of :issue, :from, :to
       record_tags = Array.new
       tags = self.to.split(",")
       tags.each do |tag|
-      sura=tag.split(":")[0]
-      aya=tag.split(":")[1]
-      verse = Verse.find_by_sura_and_aya(sura,aya)
-      return errors.add(:tags,"No Such reference exist") if verse.blank?
-      record_tags.push(verse.id)
+        sura=tag.split(":")[0]
+        aya=tag.split(":")[1]
+        verse = Verse.find_by_sura_and_aya(sura,aya)
+        return errors.add(:tags,"No Such reference exist") if verse.blank?
+        record_tags.push(verse.id)
       end
       self.verse_ids_to = record_tags.join(",")
     end
