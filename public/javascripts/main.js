fc4ed292e254f3b03b3af3556386f2bb08dd9ca8
Tauqeer Ahmad <tauqeer.ahmad@square63.com>
2012-08-01 14:19:46 +0500

    CKeditor: customized toolbar and styling in ol an ul and other styling issues.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   if($('#editor').length > 0)
-    CKEDITOR.replace('editor');
+    CKEDITOR.replace( 'editor',
+	{
+		toolbar :
+		[
+		  { name: 'basicstyles', items : [ 'Bold','Italic' ] },
+		  { name: 'paragraph', items : ['BulletedList','NumberedList' ] },
+		  { name: 'document', items : [ 'NewPage','Preview' ] },
+		  { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		  { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+		]
+	});
 });
 
