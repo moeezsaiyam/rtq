cacefe7b9d2e04298d663bc9ed10eeb04b0467aa
Tauqeer Ahmad <tauqeer.ahmad@square63.com>
2012-08-03 14:05:33 +0500

    truncation: more lin in front of text

--- public/javascripts/jtruncate.js
+++ public/javascripts/jtruncate.js
@@ -1,21 +1,22 @@
 (function($){
 	$.fn.jTruncate = function(options) {
+	   
 		var defaults = {
-			length: 200,
-			minTrail: 200,
+			length: 300,
+			minTrail: 20,
 			moreText: "more",
 			lessText: "less",
 			ellipsisText: "...",
 			moreAni: "",
 			lessAni: ""
 		};
-
+		
 		var options = $.extend(defaults, options);
-
+	   
 		return this.each(function() {
 			obj = $(this);
 			var body = obj.html();
-
+			
 			if(body.length > options.length + options.minTrail) {
 				var splitLocation = body.indexOf(' ', options.length);
 				if(splitLocation != -1) {
@@ -23,30 +24,62 @@
 					var splitLocation = body.indexOf(' ', options.length);
 					var str1 = body.substring(0, splitLocation);
 					var str2 = body.substring(splitLocation, body.length - 1);
-					obj.html(str1 + '<span class="truncate_ellipsis">' + options.ellipsisText +
-						'</span>' + '<span class="truncate_more">' + str2 + '</span>');
+					obj.html(str1 + '<span class="truncate_ellipsis">' + options.ellipsisText +'<a href="#" class="morelink" id = "more"  >'+ options.moreText+ '</a>' + '</span>' + '<span class="truncate_more">' + str2 +'</span>');
 					obj.find('.truncate_more').css("display", "none");
-
+					
+					// insert more link
+		
+          obj.find('.lesslink').css("display", "block");
 					// set onclick event for more/less link
-					var moreLink = $('.truncate_more_link', obj);
-					var moreContent = $('.truncate_more', obj);
-					var ellipsis = $('.truncate_ellipsis', obj);
+					var moreLink = $('.morelink', obj);
+					var lessLink = $('.lesslink', obj);
+				  var truncated_text=document.getElementById("truncated");
+          var full_text=document.getElementById("full-text");
+          var less_link=document.getElementById("less");
+					//var moreContent = $('.truncate_more', obj);
+					//var ellipsis = $('.truncate_ellipsis', obj);
 					moreLink.click(function() {
-						if(moreLink.text() == options.moreText) {
-							moreContent.show(options.moreAni);
-							moreLink.text(options.lessText);
-							ellipsis.css("display", "none");
-						} else {
-							moreContent.hide(options.lessAni);
-							moreLink.text(options.moreText);
-							ellipsis.css("display", "inline");
-						}
-						return false
-					});
-				}
+							less_link.style.display ="block"
+							moreLink.css("display", "none")
+						 truncated_text.style.display="none"
+						  full_text.style.display = "block"
+							return false;
+							});	
+				less_link.onclick = (function() {
+							moreLink.css("display", "inline");
+							less_link.style.display ="none"
+							truncated_text.style.display="block"
+						  full_text.style.display = "none"
+							return false;
+							});	
+				  }
+				
 			} // end if
-
+			
 		});
 	};
 })(jQuery);
 
+function toggleMe(truncated,full,more,less){
+  var truncated_text=document.getElementById(truncated);
+  var full_text=document.getElementById(full);
+  var more_link=document.getElementById(more);
+  var less_link=document.getElementById(less);
+  if(!truncated_text || !full_text)
+    return true;
+
+  if(truncated_text.style.display=="none"){
+    truncated_text.style.display="block"
+    full_text.style.display = "none"
+    more_link.style.display = "none"
+    less_link.style.display = "block"
+  }
+  else
+  {
+    truncated_text.style.display="none"
+    full_text.style.display="block"
+    more_link.style.display = "block"
+    less_link.style.display = "none"
+  }
+  return true;
+};
