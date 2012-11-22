7b179c106197e0bc95fc9c2df5a31b8f48bfd374
Tauqeer Ahmad <tauqeer.ahmad@square63.com>
2012-11-22 16:42:30 +0500

    Ckeditor: Font styling issue fixed.

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -7,5 +7,7 @@ CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.uiColor = '#AADC6E';
+  config.forcePasteAsPlainText = true;
+  config.contentsLangDirection = 'ltr';
 };
