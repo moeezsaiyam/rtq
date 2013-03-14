747c9751b6976ce43cedabb065fbc160dea434e9
Fahad Idrees <fahad.idrees@square63.com>
2013-03-14 18:26:31 +0500

    Minor Fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   # GET /images
   # GET /images.xml
   def index
-    @images = Image.all
+    @images = Image.ordered.all
 
     respond_to do |format|
       format.html # index.html.erb
